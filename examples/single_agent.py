2b0c0fe88bb8ee94245b1e2d2d5c3fe0aec067a7
Tucker <montgomery.alban@huawei.com>
2021-04-29 18:24:12 -0400

    Rename agent

--- examples/single_agent.py
+++ examples/single_agent.py
@@ -25,7 +25,7 @@ logging.basicConfig(level=logging.INFO)
 AGENT_ID = "Agent-007"
 
 
-class ChaseViaPointsAgent(Agent):
+class UTurnAgent(Agent):
     def __init__(self):
         self.lane_index = 1
         self._task_is_triggered = False
@@ -346,7 +346,7 @@ def main(scenarios, sim_name, headless, num_episodes, seed, max_episode_steps=No
         interface=AgentInterface.from_type(
             AgentType.StandardWithAbsoluteSteering, max_episode_steps=max_episode_steps
         ),
-        agent_builder=ChaseViaPointsAgent,
+        agent_builder=UTurnAgent,
     )
 
     env = gym.make(
@@ -364,7 +364,7 @@ def main(scenarios, sim_name, headless, num_episodes, seed, max_episode_steps=No
         # envision_record_data_replay_path="./data_replay",
     )
     global vvv
-    ChaseViaPointsAgent.sim = env._smarts
+    UTurnAgent.sim = env._smarts
     print(env._smarts, "::::::::::::::::::::::::::")
 
     for episode in episodes(n=num_episodes):
