ad93924a772687ca35b9a741bcbdfcc03cb08aaa
adaickalavan <adaickalavan@gmail.com>
2021-11-03 17:34:45 -0400

    Fix missing input argument to PPOGAE.

--- examples/auto_drive/train_gae.py
+++ examples/auto_drive/train_gae.py
@@ -60,7 +60,7 @@ def main(config):
     # Create model
     print("[INFO] Creating model")
     policy = ppo_gae.PPOGAE(
-        behaviour.Behaviour.CRUISER, config, config["env_para"]["seed"] + 1
+        behaviour.Behaviour.CRUISER, config, env.agent_ids, config["env_para"]["seed"] + 1
     )
 
     def interrupt(*args):
