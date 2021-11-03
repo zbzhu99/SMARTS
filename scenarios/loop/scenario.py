cfb4a3d81558750ca20ee6af69e83b96258d1471
adaickalavan <adaickalavan@gmail.com>
2021-11-03 00:37:33 -0400

    Removed social agent from loop scenario.

--- scenarios/loop/scenario.py
+++ scenarios/loop/scenario.py
@@ -33,18 +33,18 @@ laner_actor = t.SocialAgentActor(
 gen_scenario(
     t.Scenario(
         traffic={"basic": traffic},
-        social_agent_missions={
-            "all": ([laner_actor, open_agent_actor], [t.Mission(route=t.RandomRoute())])
-        },
-        bubbles=[
-            t.Bubble(
-                zone=t.PositionalZone(pos=(50, 0), size=(10, 15)),
-                margin=5,
-                actor=open_agent_actor,
-                follow_actor_id=t.Bubble.to_actor_id(laner_actor, mission_group="all"),
-                follow_offset=(-7, 10),
-            ),
-        ],
+        # social_agent_missions={
+        #     "all": ([laner_actor, open_agent_actor], [t.Mission(route=t.RandomRoute())])
+        # },
+        # bubbles=[
+        #     t.Bubble(
+        #         zone=t.PositionalZone(pos=(50, 0), size=(10, 15)),
+        #         margin=5,
+        #         actor=open_agent_actor,
+        #         follow_actor_id=t.Bubble.to_actor_id(laner_actor, mission_group="all"),
+        #         follow_offset=(-7, 10),
+        #     ),
+        # ],
     ),
     output_dir=Path(__file__).parent,
 )
