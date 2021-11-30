9737fb3d0ccf9442d81eb225fbd62808d42ff993
adaickalavan <adaickalavan@gmail.com>
2021-11-30 16:10:31 -0500

    Update

--- examples/driving_in_traffic/scenarios/loop/scenario.py
+++ examples/driving_in_traffic/scenarios/loop/scenario.py
@@ -1,4 +1,3 @@
-# import random
 from pathlib import Path
 
 from smarts.sstudio import gen_scenario
@@ -26,30 +25,9 @@ traffic = t.Traffic(
     ]
 )
 
-# open_agent_actor = t.SocialAgentActor(
-#     name="open-agent", agent_locator="open_agent:open_agent-v0"
-# )
-
-# laner_actor = t.SocialAgentActor(
-#     name="keep-lane-agent",
-#     agent_locator="zoo.policies:keep-lane-agent-v0",
-# )
-
 gen_scenario(
     t.Scenario(
         traffic={"basic": traffic},
-        # social_agent_missions={
-        #     "all": ([laner_actor, open_agent_actor], [t.Mission(route=t.RandomRoute())])
-        # },
-        # bubbles=[
-        #     t.Bubble(
-        #         zone=t.PositionalZone(pos=(50, 0), size=(10, 15)),
-        #         margin=5,
-        #         actor=open_agent_actor,
-        #         follow_actor_id=t.Bubble.to_actor_id(laner_actor, mission_group="all"),
-        #         follow_offset=(-7, 10),
-        #     ),
-        # ],
     ),
     output_dir=Path(__file__).parent,
 )
