390f562b98d59e362f4e708e36a8244629637063
adaickalavan <adaickalavan@gmail.com>
2022-02-01 21:04:26 -0500

    Set max steps.

--- examples/single_agent.py
+++ examples/single_agent.py
@@ -35,7 +35,7 @@ class ChaseViaPointsAgent(Agent):
 def main(scenarios, headless, num_episodes, max_episode_steps=None):
     agent_spec = AgentSpec(
         interface=AgentInterface.from_type(
-            AgentType.LanerWithSpeed, max_episode_steps=max_episode_steps
+            AgentType.LanerWithSpeed, max_episode_steps=max_episode_steps, rgb=True,
         ),
         agent_builder=ChaseViaPointsAgent,
     )
@@ -46,6 +46,7 @@ def main(scenarios, headless, num_episodes, max_episode_steps=None):
         agent_specs={"SingleAgent": agent_spec},
         headless=headless,
         sumo_headless=True,
+        visdom=True,
     )
 
     # Convert `env.step()` and `env.reset()` from multi-agent interface to
