a4a6ec476b13e1689f353eed4a4713d4db4cbbd8
Rutvik Gupta <RutvikGupta@users.noreply.github.com>
2022-02-08 17:59:56 -0500

    tweak width estimates

--- scenarios/waymo/waymo.py
+++ scenarios/waymo/waymo.py
@@ -235,7 +235,7 @@ class Lane:
             boundary_width = self.compute_width()
             width = max(sum(boundary_width[0]), sum(boundary_width[1]), 3.5)
             print(width)
-            return 3.5
+            return width
 
         dist_to_left_lane = 0
         dist_to_right_lane = 0
@@ -259,8 +259,7 @@ class Lane:
             dist_to_left_lane = norm(n_point[0] - self_point[0], n_point[1] - self_point[1])
 
         width = max(dist_to_left_lane, dist_to_right_lane, 3.5)
-        print(width)
-        return width
+        return 3.5
 
     def get_lane_shape(self):
         lane_width = self.get_lane_width()
