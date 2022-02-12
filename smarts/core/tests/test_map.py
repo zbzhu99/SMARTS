85526fc0129d7112a80a9124b759285e4485c3a7
Rutvik Gupta <RutvikGupta@users.noreply.github.com>
2022-02-11 20:21:07 -0500

    fix tests

--- smarts/core/tests/test_map.py
+++ smarts/core/tests/test_map.py
@@ -691,7 +691,7 @@ def test_waymo_map():
 
     offset = refline_pt.s
     width, conf = l1.width_at_offset(offset)
-    assert round(width, 2) == 3.12
+    assert round(width, 2) == 4.43
     assert conf == 1.0
     assert round(l1.curvature_radius_at_offset(offset), 2) == -291.53
     assert l1.contains_point(point)
