7f740118ae1bc881adf7b81ae20fe5f05f3623a9
qianyi-sun <sqy201x@hotmail.com>
2022-03-14 10:33:08 -0400

    add testing scenario

--- scenarios/test_scenarios/single_following/scenario.py
+++ scenarios/test_scenarios/single_following/scenario.py
@@ -5,23 +5,26 @@ from smarts.core import seed
 from smarts.sstudio import gen_scenario
 from smarts.sstudio import types as t
 
-seed(42)
+seed(4)
+# start_offset = 0
+# route1 = t.Route(begin=('-E5',0,start_offset), end=('-E10',0,'max'))
 
-start_offset = 0
-route1 = t.Route(begin=('-E5',0,start_offset), end=('-E10',0,'max'))
+rand_routes = [t.RandomRoute() for _ in range(15)]
 
-SECS_PER_HOUR = 60.0 * 60.0
-secs_btw_adding = 10
+# SECS_PER_HOUR = 60.0 * 60.0
+# secs_btw_adding = 40
+flow_prob = 1/14
 actor = t.TrafficActor(name="car",vehicle_type='passenger')
 
 traffic = t.Traffic(
     flows=[
         t.Flow(
-            route=route1,
-            actors={actor:1},
-            rate=SECS_PER_HOUR,
-            begin=randrange(secs_btw_adding)
+            route=route,
+            actors={actor:flow_prob},
+            rate=100,
+            begin=0
         )
+        for route in rand_routes
     ]
 )
 
