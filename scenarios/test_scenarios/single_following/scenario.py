55ddbc2eceaa2f6955b6a8f7180ee02b96202d6d
qianyi-sun <sqy201x@hotmail.com>
2022-03-14 11:18:42 -0400

    improve scenario

--- scenarios/test_scenarios/single_following/scenario.py
+++ scenarios/test_scenarios/single_following/scenario.py
@@ -1,36 +1,95 @@
 from pathlib import Path
-from random import randrange
 
-from smarts.core import seed
 from smarts.sstudio import gen_scenario
 from smarts.sstudio import types as t
 
-seed(4)
-# start_offset = 0
-# route1 = t.Route(begin=('-E5',0,start_offset), end=('-E10',0,'max'))
+flow1 = [t.Flow(
+            route=t.Route(
+                begin=("-E5", lane, "random"), end=("-E10", lane, "max")
+            ),
+            rate=1,
+            actors={t.TrafficActor("car"): 1},
+            
+        ) for lane in range(2)
+        for _ in range(2)]
 
-rand_routes = [t.RandomRoute() for _ in range(15)]
+flow2 = [t.Flow(
+            route=t.Route(
+                begin=("-E6", lane, "random"), end=("E11", lane, "max")
+            ),
+            rate=1,
+            actors={t.TrafficActor("car"): 1},
+            
+        ) for lane in range(2)
+        for _ in range(1)]
 
-# SECS_PER_HOUR = 60.0 * 60.0 
-# secs_btw_adding = 40
-flow_prob = 1/14
-actor = t.TrafficActor(name="car",vehicle_type='passenger')
+flow3 = [t.Flow(
+            route=t.Route(
+                begin=("-E11", lane, "random"), end=("E6", lane, "max")
+            ),
+            rate=1,
+            actors={t.TrafficActor("car"): 1},
+            
+        ) for lane in range(2)
+        for _ in range(1)]
+
+flow4 = [t.Flow(
+            route=t.Route(
+                begin=("E9", lane, "random"), end=("E4", lane, "max")
+            ),
+            rate=1,
+            actors={t.TrafficActor("car"): 1},
+            
+        ) for lane in range(2)
+        for _ in range(1)]
+
+flow5 = [t.Flow(
+            route=t.Route(
+                begin=("-E4", lane, "random"), end=("-E9", lane, "max")
+            ),
+            rate=1,
+            actors={t.TrafficActor("car"): 1},
+            
+        ) for lane in range(2)
+        for _ in range(1)]
+
+flow6 = [t.Flow(
+            route=t.Route(
+                begin=("E10", lane, "random"), end=("E5", lane, "max")
+            ),
+            rate=1,
+            actors={t.TrafficActor("car"): 1},
+            
+        ) for lane in range(2)
+        for _ in range(1)]
+
+flow7 = [t.Flow(
+            route=t.Route(
+                begin=("-E8", lane, "random"), end=("-E7", lane, "max")
+            ),
+            rate=1,
+            actors={t.TrafficActor("car"): 1},
+            
+        ) for lane in range(2)
+        for _ in range(1)]
+
+flow8 = [t.Flow(
+            route=t.Route(
+                begin=("E7", lane, "random"), end=("E8", lane, "max")
+            ),
+            rate=1,
+            actors={t.TrafficActor("car"): 1},
+            
+        ) for lane in range(2)
+        for _ in range(1)]
 
 traffic = t.Traffic(
-    flows=[
-        t.Flow(
-            route=route,
-            actors={actor:flow_prob},
-            rate=100,
-            begin=0
-        )
-        for route in rand_routes
-    ]
+    flows=flow1+flow2+flow3+flow4+flow5+flow6+flow7+flow8
 )
 
 gen_scenario(
-    t.Scenario(
-        traffic={'basic':traffic},
+    scenario=t.Scenario(
+        traffic={"basic": traffic},
     ),
     output_dir=Path(__file__).parent,
 )
